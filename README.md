5f2e42db95d021bea727a765a224de45a74ab546
Nicolas <millt.nico@hotmail.fr>
2017-03-23 10:35:53 +0100

    Delete project

--- README.md
+++
@@ -1,4 +0,0 @@
-.checkout
-=========
-
-A Symfony project created on December 11, 2016, 1:29 am.
--- src/DiabeteBundle/DiabeteBundle.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace DiabeteBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DiabeteBundle extends Bundle {
-}
