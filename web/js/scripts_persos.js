be757103d34fa386f5bcea74bb3418367c28b294
Nicolas <millt.nico@hotmail.fr>
2017-07-11 22:22:51 +0200

    Add js to form ratios

--- web/js/scripts_persos.js
+++ web/js/scripts_persos.js
@@ -2,63 +2,67 @@
  * Created by millt on 31/10/2016.
  */
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-   if ($('ol#dates li').data('hourend')) {
-      $('#formPlagesHoraires input[name="hourStart"]').val($('ol#dates li:last-child').data('hourend'));
-      $('#formPlagesHoraires input[name="hourEnd"]').val($('ol#dates li:last-child').data('hourend'));
-      $('#formPlagesHoraires input[name="hourEnd"]').attr('min', $('ol#dates li:last-child').data('hourend'));
-   }
-   else {
-      $('#formPlagesHoraires input[name="hourStart"]').val('00:00');
-      $('#formPlagesHoraires input[name="hourEnd"]').val('00:00');
-   }
+    var form = $('form#formPlagesHoraires');
+    if ($('ol#dates li').data('hourend')) {
+        var lastChildData = $('ol#dates li:last-child');
 
-
-   $('form#formPlagesHoraires #addPlage').click(function addPlage() {
-      if ($('#formPlagesHoraires input[name=hourStart]').val() != "" && $('#formPlagesHoraires input[name=hourEnd]').val() != "" && $('#formPlagesHoraires input[name=ratio]').val() != "") {
-
-         var html =
-            '<li data-hourStart="' +
-            $('#formPlagesHoraires input[name=hourStart]').val() +
-            '" data-hourEnd="' + $('#formPlagesHoraires input[name=hourEnd]').val() +
-            '" data-ratio="' +
-            $('#formPlagesHoraires input[name=ratio]').val() +
-            '">';
-         html += 'Heure start : ' + $('#formPlagesHoraires input[name=hourStart]').val() + ' / heure end : ' + $('#formPlagesHoraires input[name=hourEnd]').val() + ' / ratio : ' + $('#formPlagesHoraires input[name=ratio]').val();
-         html += '</li>';
-
-         $("#formPlagesHoraires input").each(function(index, element) {
-            this.value = "";
-         });
-
-         $('#dates').append(html);
-         $('#formPlagesHoraires input[name="hourStart"]').val($('ol#dates li:last-child').data('hourend'));
-         $('#formPlagesHoraires input[name="hourEnd"]').val($('ol#dates li:last-child').data('hourend'));
-         $('#formPlagesHoraires input[name="hourEnd"]').attr('min', $('ol#dates li:last-child').data('hourend'));
-      }
-   });
-
-   $("#formPlagesHoraires").submit(function(event) {
-
-      // Stop form from submitting normally
-      event.preventDefault();
-
-      var allData = new Array;
+        form.find('input[name="hourStart"]').val(lastChildData.data('hourend'));
+        form.find('input[name="hourEnd"]').val(lastChildData.data('hourend'));
+        form.find('input[name="hourEnd"]').attr('min', lastChildData.data('hourend'));
+    }
+    else {
+        form.find('input[name="hourStart"]').val('00:00');
+        form.find('input[name="hourEnd"]').val('00:00');
+    }
 
 
-      $('#dates li').each(function(index, element) {
-         allData.push($(this).data());
-      });
+    $('form#formPlagesHoraires #addPlage').click(function addPlage() {
+        if (form.find('input[name=hourStart]').val() !== "" && form.find('input[name=hourEnd]').val() !== "" && form.find('input[name=ratio]').val() !== "") {
 
-      console.log(JSON.stringify(allData));
-      allData = JSON.stringify(allData);
+            var html =
+                '<li data-hourStart="' +
+                form.find('input[name=hourStart]').val() +
+                '" data-hourEnd="' + form.find('input[name=hourEnd]').val() +
+                '" data-ratio="' +
+                form.find('input[name=ratio]').val() +
+                '">';
+            html += 'Heure start : ' + form.find('input[name=hourStart]').val() + ' / heure end : ' + form.find('input[name=hourEnd]').val() + ' / ratio : ' + $('#formPlagesHoraires input[name=ratio]').val();
+            html += '</li>';
 
-      var $form = $(this);
-      var url = $form.attr("action");
+            form.find("input").each(function (index, element) {
+                this.value = "";
+            });
 
-      $.post(url, 'data=' + allData, function(response) {
+            $('#dates').append(html);
+            var lastChildData = $('ol#dates li:last-child');
+            form.find('input[name="hourStart"]').val(lastChildData.data('hourend'));
+            form.find('input[name="hourEnd"]').val(lastChildData.data('hourend'));
+            form.find('input[name="hourEnd"]').attr('min', lastChildData.data('hourend'));
+        }
+    });
+
+    form.submit(function (event) {
+
+        // Stop form from submitting normally
+        event.preventDefault();
+
+        var allData = [];
+
+
+        $('#dates').find('li').each(function (index, element) {
+            allData.push($(this).data());
+        });
+
+        console.log(JSON.stringify(allData));
+        allData = JSON.stringify(allData);
+
+        var url = form.attr("action");
+        console.log(url);
+
+        /*$.post(url, 'data=' + allData, function (response) {
          alert(response);
-      });
-   });
+         });*/
+    });
 });
