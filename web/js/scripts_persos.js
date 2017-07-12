bb48d8c5bd4bd9f1dfd759640343cf2b2b670d80
Nicolas <millt.nico@hotmail.fr>
2017-07-12 14:56:32 +0000

    Form validate & removing last ratio in form

--- web/js/scripts_persos.js
+++ web/js/scripts_persos.js
@@ -3,9 +3,32 @@
  */
 
 jQuery(document).ready(function ($) {
+    function insertJsonIntoHiddenField(parent, elements, field){
+            var allData = [];
+    
+            parent.find(elements).each(function () {
+                allData.push($(this).data());
+            });
+    
+            allData = JSON.stringify(allData);
+            console.log(allData);
+            field.val(allData);
+    }
+    
+    function addRemoveButtonAfter(element){
+        element.append('<button type="button" class="close remove" aria-label="Close"><span aria-hidden="true">×</span></button></li>');
+    }
+    
 
-    /* Start Form glucid/Insulin ratios */
+    
+/* Start Form glucid/Insulin ratios */
+
     var form = $('#formPlagesHoraires');
+    if ($('ol#dates li').length > 0){
+        addRemoveButtonAfter($('ol#dates li:last-child'));
+    }
+    
+    /* Setting up default values for time fields */
     if ($('ol#dates li').data('hourend')) {
         var lastChildData = $('ol#dates li:last-child');
 
@@ -18,7 +41,7 @@ jQuery(document).ready(function ($) {
         form.find('input[name="hourEnd"]').val('00:00');
     }
 
-
+    /* Handler for click on "addPlage" button */
     form.find('#addPlage').click(function addPlage() {
         if (form.find('input[name=hourStart]').val() !== "" && form.find('input[name=hourEnd]').val() !== "" && form.find('input[name=ratio]').val() !== "") {
             $('#errorRatio').addClass('hidden');
@@ -30,36 +53,36 @@ jQuery(document).ready(function ($) {
                 form.find('input[name=ratio]').val() +
                 '">';
             html += 'Heure start : ' + form.find('input[name=hourStart]').val() + ' / heure end : ' + form.find('input[name=hourEnd]').val() + ' / ratio : ' + $('#formPlagesHoraires input[name=ratio]').val();
-            html += '</li>';
+            html += '<button type="button" class="close remove" aria-label="Close"><span aria-hidden="true">×</span></button></li>';
 
             form.find("input").each(function (index, element) {
                 this.value = "";
             });
 
             $('#dates').append(html);
-            var lastChildData = $('ol#dates li:last-child');
-            form.find('input[name="hourStart"]').val(lastChildData.data('hourend'));
-            form.find('input[name="hourEnd"]').val(lastChildData.data('hourend'));
-            form.find('input[name="hourEnd"]').attr('min', lastChildData.data('hourend'));
+            refreshFormRatio(form);
             
-            var allData = [];
-    
-    
-            $('#dates').find('li').each(function (index, element) {
-                allData.push($(this).data());
-            });
-    
-            allData = JSON.stringify(allData);
-            console.log(allData);
-            $('#fos_user_profile_form_glucidInsulinRatio').val(allData);
+            $('ol#dates li').find('.remove').remove();
+            addRemoveButtonAfter($('ol#dates li:last-child'));
+            
+            insertJsonIntoHiddenField($('#dates'), 'li', $('#fos_user_profile_form_glucidInsulinRatio'));
         } else {
             $('#errorRatio').removeClass('hidden');
             
         }
     });
     
-    $('#dates li').find('.remove').click(function (){
+    function refreshFormRatio(form){
+        var lastChildData = $('ol#dates li:last-child');
+        form.find('input[name="hourStart"]').val(lastChildData.data('hourend'));
+        form.find('input[name="hourEnd"]').val(lastChildData.data('hourend'));
+        form.find('input[name="hourEnd"]').attr('min', lastChildData.data('hourend'));
+    }
+    
+    $('body').delegate('#dates li .remove', 'click', function(){
         $(this).parent().remove();
+        refreshFormRatio(form);
+        insertJsonIntoHiddenField($('#dates'), 'li', $('#fos_user_profile_form_glucidInsulinRatio'));
     });
-    /* End Form glucid/Insulin ratios */
+/* End Form glucid/Insulin ratios */
 });
